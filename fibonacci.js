4f45b88403964e76e7e4e42feb4829de3137ad85
Fredrick <fredabisai@gmail.com>
2023-06-14 11:54:53 +0300

    Create question for fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,11 @@
-// Fibonacci
+/**
+  Fibonacci Sequence 
+  Problem: Given a number `n` find the first the first `n` elements of fibonacci sequence
+  In maths , the fibonacci sequence is a sequence in which each number is the sum of two preceeding ones
+  The first two numbers in the sequence are 0 and 1
+  Eg: fibonacci(3) = [0,1,1]
+       fibonacci(7) = [0,1, 1, 2, 3, 5, 8]
+ */
 function fibonacci(n) {
     const fibo = [0,1];
     for(let i=2; i < 2; i++) {
