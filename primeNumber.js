dc3c37a7d2eca2193a7eab1e611af8ff5f05f938
Fredrick <fredabisai@gmail.com>
2023-06-15 15:33:37 +0300

    Implement isPrime number

--- primeNumber.js
+++ primeNumber.js
@@ -6,3 +6,17 @@
   isPrime(5) = true (1*5 or 5*1)
   isPrime(4) = false (1*4 or 2*2 or 4*1)
  */
+  function isPrime(n) {
+    if(n < 2 ) {
+      return false;
+    }
+    for(let i = 2; i < n; i++) {
+       if(n%i === 0) {
+         return false;
+       }
+  
+    }
+      return true;
+  }
+  console.log("5: " + isPrime(5));
+  console.log("9: " + isPrime(9));
